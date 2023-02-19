7917a301d144c58e22f0f3b7123f6152f4d4864e
misha-rab-ymniy <mihaildubinka@gmail.com>
2023-02-19 13:31:40 +0300

    add find_even script

--- lab1/src/main.py
+++ lab1/src/main.py
@@ -1,4 +1,5 @@
 from calculations import calculate, Operation
+from find_even import find_even
 from hello_world import hello_world
 
 
@@ -9,6 +10,8 @@ def main():
         print(calculate(6, 4, Operation.MULT))
     except ZeroDivisionError as err:
         print("Division by zero")
+
+    print(find_even([2, 3, 4, 8, 7, 6, 9, 5]))
 
 
 if __name__ == "__main__":
