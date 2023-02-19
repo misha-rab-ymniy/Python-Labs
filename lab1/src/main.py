af57955142faffd2f41181891cffd864d6c64938
misha-rab-ymniy <mihaildubinka@gmail.com>
2023-02-19 13:07:37 +0300

    add calculation script

--- lab1/src/main.py
+++ lab1/src/main.py
@@ -1,8 +1,14 @@
+from calculations import calculate, Operation
 from hello_world import hello_world
 
 
 def main():
     hello_world()
+
+    try:
+        print(calculate(6, 4, Operation.MULT))
+    except ZeroDivisionError as err:
+        print("Division by zero")
 
 
 if __name__ == "__main__":
