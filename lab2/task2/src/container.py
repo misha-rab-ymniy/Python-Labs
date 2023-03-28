8b5795378ee3f922115ef0045e355cbd41380c4f
misha-rab-ymniy <mihaildubinka@gmail.com>
2023-03-28 17:57:38 +0300

    implement basic function of container

---
+++ lab2/task2/src/container.py
@@ -0,0 +1,26 @@
+import re
+
+
+class Container:
+    __storage: set[str] = set()
+
+    def add(self, key: str):
+        self.__storage.add(key)
+
+    def remove(self, key: str):
+        self.__storage.remove(key)
+
+    def find(self, key: str):
+        return key in self.__storage
+
+    def list(self):
+        return list(self.__storage)
+
+    def grep(self, reg: str):
+        return list(filter(lambda key: re.match(reg, key), self.__storage))
+
+    def save(self):
+        pass
+
+    def load(self):
+        pass
