fcc964e46dae8dc23ffc4d9777a3191a0c0808e4
misha-rab-ymniy <mihaildubinka@gmail.com>
2023-03-28 12:03:33 +0300

    add checking for abbreviations

---
+++ lab2/task1/src/abbreviations.py
@@ -0,0 +1,11 @@
+ONE_LETTER_ABB = (
+    r'Mr\.', r'Mrs\.', r'Dr\.', r'Rev\.', r'Mt\.', r'Sen\.', r'Inc\.', r'Ltd\.',
+    r'Jr\.', r'Jan\.', r'Sun\.', r'abbr\.', r'adj\.', r'Prof\.', r'adv\.', r'Eng\.',
+    r'Asst\.', r'Corp\.', r'et al\.', r'Ave\.', r'trans\.', r'Gen\.', r'Maj\.',
+    r'Hon\.', r'Ger\.', r'Pres\.', r'St\.', r'Co\.', r'Brig\.'
+)
+
+TWO_LETTER_ABB = (
+    r'e\.g\.', r'p\.m\.', r'a\.m\.', r'U\.K\.', r'p\.p\.', r'i\.e\.', r'U\.S\.',
+    r'p\.p\.', r't\.p\.', r'v\.t\.', r'v\.i\.'
+)
--- lab2/task1/src/calc_stat.py
+++ lab2/task1/src/calc_stat.py
@@ -1,7 +1,18 @@
 import re
 
+from abbreviations import ONE_LETTER_ABB, TWO_LETTER_ABB
+
 
 def calc_sentences(text: str) -> int:
     reg = "[.?!]+"
     number_sentences = len(list(re.finditer(reg, text)))
+
+    for abb in ONE_LETTER_ABB:
+        num_abb = len(re.findall(abb, text))
+        number_sentences -= num_abb
+
+    for abb in TWO_LETTER_ABB:
+        num_abb = len(re.findall(abb, text))
+        number_sentences -= num_abb * 2
+
     return number_sentences
