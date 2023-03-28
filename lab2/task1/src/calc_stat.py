b0e0195d6e694a85aef019d9b653ea5bfb15902e
misha-rab-ymniy <mihaildubinka@gmail.com>
2023-03-28 13:12:23 +0300

    add counter of average length

--- lab2/task1/src/calc_stat.py
+++ lab2/task1/src/calc_stat.py
@@ -21,3 +21,29 @@ def calc_sentences(text: str) -> int:
 def calc_non_declarative_sentences(text: str) -> int:
     reg = r'([!?]+)'
     return len(re.findall(reg, text))
+
+
+def length_each_word(words: list[str]) -> list[int]:
+    return list(map(lambda word: len(word), words))
+
+
+def separate_words(text: str) -> list[str]:
+    reg = r'\w*[A-z]\w*'
+    return re.findall(reg, text)
+
+
+def average_sentences_length(text: str) -> float:
+    len_words = sum(length_each_word(separate_words(text)))
+    num_sentences = calc_sentences(text)
+    try:
+        return len_words / num_sentences
+    except ZeroDivisionError:
+        return 0
+
+
+def average_words_length(text: str) -> float:
+    len_each_word = length_each_word(separate_words(text))
+    try:
+        return sum(len_each_word) / len(len_each_word)
+    except ZeroDivisionError:
+        return 0
--- lab2/task1/src/main.py
+++ lab2/task1/src/main.py
@@ -1,10 +1,12 @@
-from calc_stat import calc_sentences, calc_non_declarative_sentences
+from calc_stat import calc_sentences, calc_non_declarative_sentences, average_sentences_length, average_words_length
 
 
 def main():
     text = input("Please, input the text: \n")
     print(f"Number of sentences in the text: {calc_sentences(text)}")
     print(f"Number of non-declarative sentences in the text: {calc_non_declarative_sentences(text)}")
+    print(f"Average length of the sentence in characters: {average_sentences_length(text)}")
+    print(f"Average length of the word in characters: {average_words_length(text)}")
 
 
 if __name__ == "__main__":
