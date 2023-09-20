ffaf307ebf2dad94518cc51422cd8526d80028d8
misha-rab-ymniy <mihaildubinka@gmail.com>
2023-09-20 14:08:25 +0300

    add faq page

--- lab4/info/urls.py
+++ lab4/info/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 app_name = "info"
 urlpatterns = [
-    path('', views.about_us, name='about_us'),
+    path('about_us', views.about_us, name='about_us'),
+    path('faq', views.faq, name='faq'),
 ]
--- lab4/info/views.py
+++ lab4/info/views.py
@@ -1,6 +1,10 @@
 # Create your views here.
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render
 
 
 def about_us(request):
     return render(request, 'info/about_us.html')
+
+
+def faq(request):
+    return render(request, 'faq/faq.html')
